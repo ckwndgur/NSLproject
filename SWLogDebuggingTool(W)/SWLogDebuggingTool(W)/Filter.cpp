db4d24d983f37a577be10cfd6f331afb1ef171fa
JinYong Im <bigshow0812@kumoh.ac.kr>
2017-01-10 21:12:17 +0900

    로그 전시 부분 디버깅

--- SWLogDebuggingTool(W)/SWLogDebuggingTool(W)/Filter.cpp
+++ SWLogDebuggingTool(W)/SWLogDebuggingTool(W)/Filter.cpp
@@ -2,6 +2,8 @@
 #include "StdAfx.h"
 #include "Filter.h"
 #include "FolderManager.h"
+#include "stdlib.h"
+#include "string.h"
 
 Filter::Filter()
 {
@@ -28,7 +30,129 @@ string Filter::CreatingTime(string WantedLog)
 
 	return Title;
 }
+//list<string> keywords = [1] category [2] keyword
+//string Title = [keyword]
+//CString originpath = 검색 실행 할 원본로그주소
+//CString newpath = 검색 결과 저장 할 주소
+//bool multiflag = 다중필터링 확인
+list<CString> Filter::MultiFilter(list<string> keywords, string Title, CString originpath, CString newpath, bool multiflag)
+{
+	ifstream originfile(originpath); //원본로그파일을 열어 저장합니다.
+	string alinelog, cateBuf, keywordBuf, resultlog, devidedlog;
+	alinelog = cateBuf =  keywordBuf = resultlog = devidedlog = "";
+	CString linenumberBuf;
+	linenumberBuf = "";
+	int category, valStart, valEnd, linenumber;
+	category = valStart = valEnd = linenumber = 0;
 
+	list<CString> FilteredLog;
+/*
+	ifstream originfile(originpath, ios::in); // 
+	if(originfile.fail()){
+		cout << "파일을 여는 데 실패했습니다."<< endl;
+	}
+*/
+	ofstream resultfile(newpath, ios::out);
+	if(resultfile.fail()){
+		cout << "파일을 쓰는 데 실패했습니다."<< endl;
+	}
+
+	while (!originfile.eof())
+	{
+		getline(originfile, alinelog); //원본로그파일을 한줄 씩 받아옵니다.
+		
+		cateBuf = keywords.front();
+		category = atoi(cateBuf.c_str()); //카테고리를 받아옵니다.
+		keywords.pop_front();
+		keywordBuf = keywords.front(); //키워드를 받아옵니다.
+
+		//카테고리 기반으로 로그를 분리합니다.
+		if (multiflag == false)//다중 필터링이 아닐경우
+		{
+			linenumber = linenumber + 1; //로그의 라인 넘버를 증가시킵니다.
+
+			linenumberBuf.Format(_T("%d"), linenumber); //int -> CString
+
+			CT2CA AnsiStringlinenumber(linenumberBuf); //CString -> string
+			string resultlogbuf(AnsiStringlinenumber);
+			resultlog = "";
+			resultlog = resultlogbuf + ": "; //결과로그의 각 줄 첫부분에 원본로그의 라인넘버를 입력합니다.
+
+			devidedlog = LogDivider(category, alinelog);
+			
+			alinelog = resultlog + alinelog;
+		} 
+
+		else//다중 필터링일 경우
+		{
+			valEnd  = alinelog.find(": ");
+			resultlog = alinelog.substr(0, valEnd);
+			alinelog.erase(0,valEnd+2);
+
+			devidedlog = LogDivider(category, alinelog);
+		}
+
+		//키워드가 해당 로그에 존재하는지 검사합니다.
+		if (devidedlog.find(keywordBuf))
+		{
+			resultlog = alinelog; //결과에 해당 로그를 저장합니다.
+			FilteredLog.push_back(resultlog.c_str());
+			resultfile << resultlog << endl;
+		} 
+		else
+		{
+		}
+		
+	}
+
+	return FilteredLog;
+}
+
+string Filter::LogDivider(int category, string alinelog)
+{
+	int valstart, valend, vallength;
+	valstart = valend = vallength = 0;
+	string valrtnLog;
+	
+	switch(category)
+	{
+	case 1 :
+		valend = alinelog.find(")");
+		valrtnLog = alinelog.substr(1,valend);
+		break;
+
+	case 2 :
+		valstart = alinelog.find(")[");
+		valend = alinelog.find("]__");
+		vallength = valend - valstart;
+		valrtnLog = alinelog.substr(valstart, vallength);
+		break;
+
+	case 3 :
+		valstart = alinelog.find("]__");
+		valend = alinelog.find(".");
+		valend = valend + 1;
+		vallength = valend - valstart;
+		valrtnLog = alinelog.substr(valstart, vallength);
+		break;
+
+	case 4 :
+
+		break;
+
+	case 5 :
+
+		break;
+
+	case 6 :
+
+		break;
+
+	default : AfxMessageBox(TEXT("카테고리 범주가 아닙니다."));
+		break;
+	}
+	return valrtnLog;
+}
 
 list<CString> Filter::DoFilter(int Category, string WantedLog, string Title, CString filepath, CString Rfilepath, bool multiflag)
 {
@@ -91,7 +215,7 @@ list<CString> Filter::DoFilter(int Category, string WantedLog, string Title, CSt
 
 	ofstream output(csfilepath, ios::out); 
 	if(output.fail()){
-		cout << "파일을 쓰는 데 실패했습니다.1"<< endl;
+		cout << "파일을 쓰는 데 실패했습니다."<< endl;
 	}
 	int LogPartStartPoint= 0, LogPartEndPoint =0;
 	
--- SWLogDebuggingTool(W)/SWLogDebuggingTool(W)/Filter.h
+++ SWLogDebuggingTool(W)/SWLogDebuggingTool(W)/Filter.h
@@ -2,10 +2,11 @@
 
 #include <Windows.h>
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <stdio.h>
 #include <fstream>
 #include <list>
+
 using namespace std;
 
 class Filter
@@ -14,6 +15,8 @@ public:
 	Filter();
 	~Filter();
 	list<CString> DoFilter(int Category,string WantedLog,string Title, CString filepath, CString Rfilepath, bool multiflag);
+	list<CString> MultiFilter(list<string> keywords, string Title, CString originpath, CString newpath, bool multifalg);
+	string LogDivider(int category, string alinelog);
 	string CreatingTime(string WantedLog);
 
 	string WantedLog;
